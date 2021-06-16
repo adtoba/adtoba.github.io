6a4e4f7933742f03cdd3d15d09cf7b5d118e6d76
adtoba <adetoba53@gmail.com>
2021-06-16 18:59:22 +0100

    Navigate to links

--- lib/components/app/contact_widget.dart
+++ lib/components/app/contact_widget.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:html' as html;
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:portfolio/utils/config.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactWidget extends StatelessWidget {
   @override
@@ -11,20 +16,44 @@ class ContactWidget extends StatelessWidget {
         direction: Axis.horizontal,
         mainAxisSize: MainAxisSize.max,
         children: [
-          SvgPicture.asset(
-            'assets/icons/twitter.svg',
-            height: Config.yMargin(context, 2),
-            width: Config.xMargin(context, 2),
-            color: Colors.white,
+          IconButton(
+            onPressed: () async {
+              var repo = "https://www.twitter.com/tobacodes";
+              if (kIsWeb) {
+                html.window.open(repo, '');
+              } else {
+                if (await canLaunch(repo)) {
+                  launch(repo);
+                }
+              }
+            },
+            icon: SvgPicture.asset(
+              'assets/icons/twitter.svg',
+              height: Config.yMargin(context, 2),
+              width: Config.xMargin(context, 2),
+              color: Colors.white,
+            ),
           ),
           SizedBox(
             width: Config.xMargin(context, 1.5),
           ),
-          SvgPicture.asset(
-            'assets/icons/github.svg',
-            height: Config.yMargin(context, 2),
-            width: Config.xMargin(context, 2),
-            color: Colors.white,
+          IconButton(
+            onPressed: () async {
+              var repo = "https://www.github.com/adtoba";
+              if (kIsWeb) {
+                html.window.open(repo, '');
+              } else {
+                if (await canLaunch(repo)) {
+                  launch(repo);
+                }
+              }
+            },
+            icon: SvgPicture.asset(
+              'assets/icons/github.svg',
+              height: Config.yMargin(context, 2),
+              width: Config.xMargin(context, 2),
+              color: Colors.white,
+            ),
           ),
         ],
       ),
